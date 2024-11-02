05b8d623dd8376eb3cb46a8cb4abf82ae0279c3d
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-11-02 17:12:42 +0300

    Initial

---
+++ MvcProject/Controllers/UrunController.cs
@@ -0,0 +1,19 @@
+﻿using MvcProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class UrunController : Controller
+    {
+        dbMvcProjectEntities db= new dbMvcProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.Tbl_Urunler.ToList();
+            return View(values);
+        }
+    }
+}
