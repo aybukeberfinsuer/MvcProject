01550b32a16c46d38974b3b724c81881489160c3
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-11-03 18:31:38 +0300

    Crud işlenlerinin ilk aşaması yapıldı.

--- MvcProject/Controllers/KategoriController.cs
+++ MvcProject/Controllers/KategoriController.cs
@@ -15,5 +15,41 @@ namespace MvcProject.Controllers
             var values= db.Tbl_Kategoriler.ToList();
             return View(values);
         }
-    }
+
+        [HttpGet]
+		public ActionResult Ekle()
+		{		
+			return View();
+		}
+
+		[HttpPost]
+        public ActionResult Ekle(Tbl_Kategoriler category1)
+        {           
+            db.Tbl_Kategoriler.Add(category1);
+            db.SaveChanges();
+            return RedirectToAction("Index");           
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var kategori= db.Tbl_Kategoriler.Find(id);
+            db.Tbl_Kategoriler.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir( int id)
+        {
+            var kategori = db.Tbl_Kategoriler.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        public ActionResult Guncelle(Tbl_Kategoriler kategori)
+        {
+            var ktg= db.Tbl_Kategoriler.Find(kategori.KategoriId);
+            ktg.KategoriAd = kategori.KategoriAd;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+	}
 }
--- MvcProject/Controllers/MusteriController.cs
+++ MvcProject/Controllers/MusteriController.cs
@@ -1,4 +1,5 @@
-﻿using MvcProject.Models.Entity;
+﻿using Microsoft.Ajax.Utilities;
+using MvcProject.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,43 @@ namespace MvcProject.Controllers
             var values= db.Tbl_Musteriler.ToList();
             return View(values);
         }
-    }
+
+		[HttpGet]
+		public ActionResult Ekle()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Ekle(Tbl_Musteriler musteri)
+		{
+			db.Tbl_Musteriler.Add(musteri);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Sil(int id)
+		{
+			var silinecek_musteri = db.Tbl_Musteriler.Find(id);
+			db.Tbl_Musteriler.Remove(silinecek_musteri);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+
+		public ActionResult MusteriGetir( int id)
+		{
+			var musteri= db.Tbl_Musteriler.Find(id);
+			return View("MusteriGetir", musteri);
+		}
+
+		public ActionResult Guncelle(Tbl_Musteriler p1)
+		{
+			var guncel_musteri = db.Tbl_Musteriler.Find(p1.MusteriId);
+			guncel_musteri.MusteriAd= p1.MusteriAd;
+			guncel_musteri.MusteriSoyad= p1.MusteriSoyad;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
